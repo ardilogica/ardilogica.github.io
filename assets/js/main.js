601528602187ee8ed40c27197ce3b9ccea9c5c78
ArdiSandra <ardilogica@gmail.com>
2021-12-11 23:51:38 +0700

    Penambahan audio first

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,8 +89,7 @@ function scrollUp() {
   else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+// /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
@@ -236,4 +235,27 @@ function play() {
 
 window.onload = function () {
   play();
+};
+
+const myAudio = document.getElementById('audio');
+const btn = document.getElementById('btn-playback');
+const btnIcon = document.querySelector('#btn-playback > i');
+const btnText = document.getElementById('btn-text');
+
+const toggleMusic = () => {
+  const dataPlay = btn.getAttribute('data-play');
+
+  if (dataPlay === 'false') {
+    btn.setAttribute('data-play', 'true');
+    myAudio.play();
+    btnIcon.classList.remove('fa-music');
+    btnIcon.classList.add('fa-pause-circle');
+    // btnText.innerText = 'PAUSE';
+  } else {
+    btn.setAttribute('data-play', 'false');
+    myAudio.pause();
+    btnIcon.classList.remove('fa-pause-circle');
+    btnIcon.classList.add('fa-music');
+    // btnText.innerText = 'PLAY';
+  }
 };
